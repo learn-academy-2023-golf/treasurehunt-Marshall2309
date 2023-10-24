dc75e8db5511ec12c9e7904bce790c566042a3eb
Christopher <ckmarshal23@gmail.com>
2023-10-24 14:26:46 -0700

    Tree-emojoi

--- src/App.js
+++ src/App.js
@@ -15,6 +15,13 @@ const App = () => {
     "?"
   ])
 
+  const handleGamePlay = (index) => {
+    // alert(index)
+    let updatedBoard = [...board]
+    updatedBoard[index] = "🌴"
+    setBoard(updatedBoard)
+  }
+
   return (
     <>
       <h1>Treasure Hunt Game</h1>
@@ -24,6 +31,8 @@ const App = () => {
         <Square 
         value={value}
         key={index}
+        index={index}
+        handleGamePlay={handleGamePlay}
         />
         )
       })}
