b8f16b2e20975cb01c64d3b40e8edaae7bda48df
Christopher <ckmarshal23@gmail.com>
2023-10-25 08:59:15 -0700

    Finished Tresure Hunt

--- src/App.js
+++ src/App.js
@@ -14,12 +14,35 @@ const App = () => {
     "?",
     "?"
   ])
+  const [treasureLocation, setTreasureLocation] = useState(Math.floor(Math.random() * board.length))
+  const [bombLocation, setBombLocation] = useState(Math.floor(Math.random() * board.length))
+
+  console.log("treasure:", treasureLocation)
+  console.log("bomb:",bombLocation)
 
   const handleGamePlay = (index) => {
     // alert(index)
     let updatedBoard = [...board]
-    updatedBoard[index] = "🌴"
+    if(treasureLocation === index) {
+      updatedBoard[index] = <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTat9Qr3qGDrSzw6TtxMA6C9-D8C38zSieU0Q&usqp=CAU" alt="Treasure"
+      style={{ width: '100%', height: '100%' }}
+      />
     setBoard(updatedBoard)
+  } else if (bombLocation === index) {
+    updatedBoard[index] = <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT5En1Hwuj2ZnYZoBXAqcZwZ-LEIDVT4tkJ8A&usqp=CAU" alt="Coronavirus"
+    style={{ width: '100%', height: '100%' }}
+    />
+    setBoard(updatedBoard)
+  } else {
+    updatedBoard[index] = <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTuzb70ezSpJbRw-UN159fW0JLTcNYFOkEg98ZnI0ly4MVBowSzofHWa_FdXtUOGMYZbVg&usqp=CAU" alt="Treasure"
+    style={{ width: '100%', height: '100%' }}
+    />
+    setBoard(updatedBoard)
+  }
+  }
+
+  const resetGame = () => {
+    window.location.reload();
   }
 
   return (
@@ -37,6 +60,7 @@ const App = () => {
         )
       })}
     </div>
+    <button onClick={resetGame}>Play Again</button>
     </>
   )
 }
